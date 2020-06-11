2933be081d2607051c67fd445657dbdde5ea4e3d
DmitryKovtun <dm_ko@outlook.com>
2020-06-11 16:40:15 +0300

    add expand_json method for expanding json list to rows

--- src/commands/base_export.py
+++ src/commands/base_export.py
@@ -40,14 +40,54 @@ class BaseExport(BaseCommand):
         self.field_mapping = {}
 
         #
-        self.allow_json = True
+        self.allow_json = False
         #
         # how much fetches to do for each file (self.max_file_length)
         self.fetching_times = 100
+        # place, where mapping will be
+        self.columns = {}
+        #
+        # list of json columns in file
+        # will be used for extending rows
+        self.json_columns = []
+        #
+        # defines if json-extended rows will be empty
+        self.is_new_row_empty = True
 
     def init(self) -> None:
         """Fields initialization before self.run()"""
-        pass
+        self.columns = {
+            "id": "id",
+            "search_id": "search_id",
+            # "company_id": "company_id",
+            "url": "url",
+            "ln_id": "ln_id",
+            "first_name": "first_name",
+            "last_name": "last_name",
+            "title": "title",
+            "location": "location",
+            "connections_count": "connections_count",
+            "summary": "summary",
+            "industry_id": "industry_id",
+            # "current_positions": "current_positions",
+            "positions": "positions",
+            "educations": "educations",
+            "skills": "skills",
+            "interests": "interests",
+            # "error": "error",
+            # "status": "status",
+            # "sent_to_customer": "sent_to_customer",
+            # "created_at": "created_at",
+            # "updated_at": "updated_at",
+        }
+        # list some json columns (database names)
+        self.json_columns = [
+            # "current_positions",
+            "positions",
+            "educations",
+            # "skills",
+            # "interests",
+        ]
 
     def add_options(self, parser) -> None:
         super().add_options(parser)
@@ -59,15 +99,12 @@ class BaseExport(BaseCommand):
         # экспорта плоской таблицы/результата SELECT запроса к БД.
 
         # Требования к команде:
-
         # - возможность выбор варианта экспорта:
         #     - c  строками в одной ячейке
         #     - с вертикальным развертыванием  массивов. Как минимум одно поле, которое будет разворачиваться
         #           - в случае выбора опции вертикального развертывания также доступна опция выбора заполненности таблицы:
         #               - дублирование - для каждой развернутой из  записи дублируется вся остальная информация
         #               - облегченная - информация о присутствует только для первой из записей, для остальных должны быть пустые ячейки
-        # your code here
-
         # - возможность выбор количества записей для экспорта
 
         # - возможность выбора количества записей в одном файле. По умолчанию 100000.
@@ -114,19 +151,26 @@ class BaseExport(BaseCommand):
             except IndexError:
                 # OK
                 pass
-            self.logger.info(f"previous_id: {previous_id}")
+            self.logger.info("previous_id: %s", previous_id)
+
+        # TODO duplicate code fix
+        # TODO filename generation
+        filename = f"temp_{fetched_rows}"
+        # 'yield' file with data
+        stats[filename] = len(output_data) - 1
+        self.export_data(output_data, filename)
 
         # TODO stats here
         for filename in stats:
-            self.logger.info(f"File: '{filename}', {stats[filename]} items")
-        self.logger.info(f"Exported {fetched_rows} rows!")
+            self.logger.info("File: '%s', %s items", filename, stats[filename])
+        self.logger.info("Exported %s rows!", fetched_rows)
         self.logger.info("Export done")
 
     def fetch_data(self, **kwargs):
         query = text(
             """
             select * from members
-            where status in :statuses and id > :previous_id
+            where status in :statuses and id > :previous_id and id = 147
             limit :take
             """
         )
@@ -178,53 +222,11 @@ class BaseExport(BaseCommand):
             List[tuple]: [description]
         """
 
-        #
-        # rename a field
-        # mappings["subject_id"] = "id"
-        # translate a field
-        # mappings["gender"] = "sex", {"male": "M", "female": "F"}
-        # apply a calculation to a field
-        # mappings["age_months"] = "age", lambda v: v * 12
-        # apply a calculation to a combination of fields
-        # mappings["bmi"] = lambda rec: rec["weight"] / rec["height"] ** 2
-        # transform and inspect the output
         self.mappings = OrderedDict()
-        self.columns = {
-            "id": "id",
-            "search_id": "search_id",
-            # "company_id": "company_id",
-            "url": "url",
-            "ln_id": "ln_id",
-            "first_name": "first_name",
-            "last_name": "last_name",
-            "title": "title",
-            "location": "location",
-            "connections_count": "connections_count",
-            "summary": "summary",
-            "industry_id": "industry_id",
-            # "current_positions": "current_positions",
-            "positions": "positions",
-            "educations": "educations",
-            "skills": "skills",
-            "interests": "interests",
-            # "error": "error",
-            # "status": "status",
-            # "sent_to_customer": "sent_to_customer",
-            # "created_at": "created_at",
-            # "updated_at": "updated_at",
-        }
         for col in self.columns:
             self.mappings[self.columns[col]] = col
 
-        # list some json columns (database names)
-        json_columns = [
-            # "current_positions",
-            "positions",
-            "educations",
-            "skills",
-            "interests",
-        ]
-        for col in json_columns:
+        for col in self.json_columns:
             self.mappings[self.columns[col]] = col, lambda value: json.loads(value)
         # TODO custom overrides
 
@@ -233,9 +235,81 @@ class BaseExport(BaseCommand):
     def prepare_data(self, data_dict) -> DataFrame:
         pass
 
-    def process_data(self, data_dict) -> DataFrame:
+    def expand_json(self, output_data):
+        expanding_map = {
+            "positions": [
+                "title",
+                "company",
+                # "company": {
+                #    "url": "https://www.linkedin.com/company/banco-bai/",
+                #    "name": "BAI - Banco Angolano de Investimentos, s.a.",
+                #    "size": "1001-5000",
+                #    "ln_id": 976135,
+                #    "industry_id": 40,
+                # },
+                "end_date",
+                "start_date",
+            ],
+            "educations": [],
+        }
 
-        return DataFrame(data_dict)
+        # new_columns = self.columns.copy()
+        # header
+        new_output = [output_data[0]]
+        #
+        # get some memory for new columns
+        # expanded_lists = {}
+        # for expanded in expanding_map:
+        #    if expanded in new_columns:
+        #        new_columns.pop(expanded)
+        #
+        #    for inner_json in expanding_map[expanded]:
+        #        col_name = f"{expanded}_{inner_json}"
+        #        new_columns[col_name] = col_name
+        #        expanded_lists[col_name] = []
+
+        empty_row = ["" for _ in self.columns]
+        for data_row in output_data[1:]:
+            new_data_rows = []
+
+            data_row = list(data_row)
+            data_row_no_json = data_row.copy()
+            for expanded in self.json_columns:
+                column_index = list(self.columns).index(expanded)
+                data_row_no_json[column_index] = ""
+            new_data_rows.append(data_row_no_json)
+
+            # expand rows with json list items
+            for expanded in self.json_columns:
+                column_index = list(self.columns).index(expanded)
+
+                for list_item in data_row[column_index]:
+                    needed_row_index = None
+                    # find empty row
+                    for index, filled_row in enumerate(new_data_rows):
+                        if not filled_row[column_index]:
+                            needed_row_index = index
+                            break
+
+                    current_row_index = None
+                    if needed_row_index is not None:
+                        current_row_index = needed_row_index
+                    else:
+                        # no empty row found, create new one
+                        current_row = []
+                        if self.is_new_row_empty:
+                            current_row = empty_row.copy()
+                        else:
+                            current_row = data_row_no_json.copy()
+                        new_data_rows.append(current_row)
+                        current_row_index = -1
+
+                    new_data_rows[current_row_index][column_index] = list_item
+            for new_data_row in new_data_rows:
+                new_output.append(new_data_row)
+
+        # self.columns = new_columns
+        return new_output
 
     def before_export(self, output_data):
         # TODO remove unused columns here
@@ -244,12 +318,17 @@ class BaseExport(BaseCommand):
         # list some json columns (database names)
         for col in self.columns:
             self.mappings[self.columns[col]] = col
-        # TODO custom overrides
 
         skip = ["id", "error", "status", "created_at", "updated_at", "sent_to_customer"]
         for s in skip:
             if self.mappings.get(s):
                 self.mappings.pop(s)
+
+        if self.allow_json:
+            for col in self.json_columns:
+                self.mappings[self.columns[col]] = col, lambda value: json.dumps(value)
+        else:
+            output_data = self.expand_json(output_data)
 
         return etl.fieldmap(output_data, self.mappings)
 
@@ -264,16 +343,13 @@ class BaseExport(BaseCommand):
         # - Сделать пример использования на основании scrapy команды.
         # - Результат отправить в отдельную ветку на github scrapy-boilerplate. Сделать PR в ветку разработки
 
-        #
-
         # - В общем и целом команда для шаблона должна быть абстрактной.
         # Что-то похожее на rmq.Producer/rmq.Consumer, чтобы при конечной реализации достаточно было определить
         # запрос для получения данных из БД/или метод предварительной подготовки данных,
         # метод маппинга полей в названия колонок файла,
         # возможно метод обновления записи в БД (поддержка поля sent_to_customer)
-        # self.logger.info(previous_id)
+
         self.file_type = self.FileTypes.csv
-        # self.logger.info(f"output_data: {output_data}")
 
         if self.file_type == self.FileTypes.csv:
             output_data.tocsv(f"{filename}.csv", "utf-8", write_header=True)
